55dea3ecedf77e50ef00d2344a7f1ab005e80871
Mike P. Sinn <m@quantimodo.com>
2018-04-30 18:16:39 -0500

    f762075eba60a5517695997944e312e5c1f0ed10

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #### halt script on error
-set -xe
+#set -xe
 
 echo '##### Print docker version'
 docker --version
@@ -11,13 +11,16 @@ env | sort
 
 git config core.sparsecheckout # timeout=10
 
-set +x
 
 echo "Fetching changes from the remote Git repository"
-git config remote.origin.url https://${GITHUB_TOKEN}@github.com/mikepsinn/QM-Docker.git # timeout=10
+set +x
+git config remote.origin.url https://${GITHUB_ACCESS_TOKEN}@github.com/mikepsinn/QM-Docker.git # timeout=10
+set -x
 echo "Fetching upstream changes from QM-Docker"
 git --version
-git fetch --no-tags --progress https://${GITHUB_TOKEN}@github.com/mikepsinn/QM-Docker.git +refs/heads/*:refs/remotes/origin/* --prune --depth=20
+set +x
+git fetch --no-tags --progress https://${GITHUB_ACCESS_TOKEN}@github.com/mikepsinn/QM-Docker.git +refs/heads/*:refs/remotes/origin/* --prune --depth=20
+set -x
 git show-ref --tags -d
 echo "Checking out Revision ${TRAVIS_COMMIT_MESSAGE}"
 git config core.sparsecheckout # timeout=10
