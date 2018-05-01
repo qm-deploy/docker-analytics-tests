7e522ca87ba7a11d304089eca4ff4d53d4dce880
Mike P. Sinn <m@quantimodo.com>
2018-05-01 01:50:11 -0500

    export SHA=$(echo ${TRAVIS_COMMIT_MESSAGE} | cut -f3 -d#)

--- travis-build.sh
+++ travis-build.sh
@@ -2,9 +2,9 @@
 export TEST_REPO_PATH="$PWD"
 export QM_DOCKER_PATH="$PWD/QM-Docker"
 echo "HOSTNAME is ${HOSTNAME} and QM_DOCKER_PATH is $QM_DOCKER_PATH"
-export TEST_SUITE=$(echo ${TRAVIS_COMMIT_MESSAGE} | cut -f1 -d'-branch-')
-export BRANCH=$(echo ${TRAVIS_COMMIT_MESSAGE} | cut -f2 -d'-branch-')
-export SHA=$(echo ${TRAVIS_COMMIT_MESSAGE} | cut -f2 -d'-sha-')
+export TEST_SUITE=$(echo ${TRAVIS_COMMIT_MESSAGE} | cut -f1 -d#)
+export BRANCH=$(echo ${TRAVIS_COMMIT_MESSAGE} | cut -f2 -d#)
+export SHA=$(echo ${TRAVIS_COMMIT_MESSAGE} | cut -f3 -d#)
 
 source ${TEST_REPO_PATH}/update-status.sh --sha=${SHA} \
    --repo=mikepsinn/QM-Docker \
