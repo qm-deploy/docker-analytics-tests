3070d8d31dc49a18ab6b399947d4332a1b611e24
Mike P. Sinn <m@quantimodo.com>
2018-04-30 22:23:33 -0500

    c3fdf3f2f9f35aaf53309ada3cafe1a0e0d1bbc3

--- travis-build-docker.sh
+++ travis-build-docker.sh
@@ -30,25 +30,36 @@ git checkout FETCH_HEAD
 
 ls
 
-export CLEARDB_DATABASE_URL=mysql://root:@127.0.0.1/${TEST_SUITE}?reconnect=true
-export CLEARDB_DATABASE_URL_READONLY=mysql://root:@127.0.0.1/${TEST_SUITE}?reconnect=true
-export TEST_CLEARDB_DATABASE_URL=mysql://root:@127.0.0.1/${TEST_SUITE}?reconnect=true
-export TEST_CLEARDB_DATABASE_URL_READONLY=mysql://root:@127.0.0.1/${TEST_SUITE}?reconnect=true
-export MONGO_DB_CONNECTION=mongodb://127.0.0.1:27017
+export CLEARDB_DATABASE_URL=mysql://root:root@mysql/${TEST_SUITE}?reconnect=true
+export CLEARDB_DATABASE_URL_READONLY=mysql://root:root@mysql/${TEST_SUITE}?reconnect=true
+export TEST_CLEARDB_DATABASE_URL=mysql://root:root@mysql/${TEST_SUITE}?reconnect=true
+export TEST_CLEARDB_DATABASE_URL_READONLY=mysql://root:root@mysql/${TEST_SUITE}?reconnect=true
+export MONGO_DB_CONNECTION=mongodb://mongo:27017
 ENV_COMMAND="export TEST_CLEARDB_DATABASE_URL=${TEST_CLEARDB_DATABASE_URL} && export TEST_CLEARDB_DATABASE_URL_READONLY=${TEST_CLEARDB_DATABASE_URL_READONLY} && export MONGO_DB_CONNECTION=${MONGO_DB_CONNECTION} && "
 mkdir ${QM_DOCKER_PATH}/phpunit || true
 
 echo "Copying slim/envs/circleci.env to .env"
 cp ${QM_DOCKER_PATH}/slim/envs/circleci.env ${QM_DOCKER_PATH}/.env
+cp ${TEST_REPO_PATH}/test.env ${QM_DOCKER_PATH}/laradock/.env
 
-cd slim && composer install
+cd ${QM_DOCKER_PATH}/laradock
+docker-compose build mysql
+docker-compose up -d mysql workspace mongo
+#echo "Waiting for mysql"
+#until mysql -hmysql -P3306 -uroot -proot &> /dev/null
+#do
+#  printf "."
+#  sleep 1
+#done
+
+docker-compose exec workspace bash -c "${ENV_COMMAND} cd slim && composer install"
 
 if [ ${TEST_SUITE} = "Laravel" ]
  then
-    cd laravel && composer install
-    slim/vendor/phpunit/phpunit/phpunit --configuration laravel/phpunit.xml --stop-on-error --stop-on-failure --log-junit phpunit/${TEST_SUITE}.xml
+    docker-compose exec workspace bash -c "${ENV_COMMAND} cd laravel && composer install"
+    docker-compose exec workspace bash -c "${ENV_COMMAND} slim/vendor/phpunit/phpunit/phpunit --configuration laravel/phpunit.xml --stop-on-error --stop-on-failure --log-junit phpunit/${TEST_SUITE}.xml"
  else
-    slim/vendor/phpunit/phpunit/phpunit --stop-on-error --stop-on-failure --configuration slim/tests/phpunit.xml --log-junit phpunit/${TEST_SUITE}.xml slim/tests/Api/${TEST_SUITE}
+    docker-compose exec workspace bash -c "${ENV_COMMAND} slim/vendor/phpunit/phpunit/phpunit --stop-on-error --stop-on-failure --configuration slim/tests/phpunit.xml --log-junit phpunit/${TEST_SUITE}.xml slim/tests/Api/${TEST_SUITE}"
 fi
 
 source ${TEST_REPO_PATH}/update-status.sh --sha=${TRAVIS_COMMIT_MESSAGE} \
