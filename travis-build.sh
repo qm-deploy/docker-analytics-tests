0ecfe49a5c26a039cb96ebfc4d258637caa4a03e
Mike P. Sinn <m@quantimodo.com>
2018-04-30 20:54:22 -0500

    c3fdf3f2f9f35aaf53309ada3cafe1a0e0d1bbc3

--- travis-build.sh
+++ travis-build.sh
@@ -34,7 +34,8 @@ export CLEARDB_DATABASE_URL=mysql://root:root@mysql/${TEST_SUITE}?reconnect=true
 export CLEARDB_DATABASE_URL_READONLY=mysql://root:root@mysql/${TEST_SUITE}?reconnect=true
 export TEST_CLEARDB_DATABASE_URL=mysql://root:root@mysql/${TEST_SUITE}?reconnect=true
 export TEST_CLEARDB_DATABASE_URL_READONLY=mysql://root:root@mysql/${TEST_SUITE}?reconnect=true
-ENV_COMMAND="export TEST_CLEARDB_DATABASE_URL=${TEST_CLEARDB_DATABASE_URL} && export TEST_CLEARDB_DATABASE_URL_READONLY=${TEST_CLEARDB_DATABASE_URL_READONLY} &&"
+export MONGO_DB_CONNECTION=mongodb://mongo:27017
+ENV_COMMAND="export TEST_CLEARDB_DATABASE_URL=${TEST_CLEARDB_DATABASE_URL} && export TEST_CLEARDB_DATABASE_URL_READONLY=${TEST_CLEARDB_DATABASE_URL_READONLY} && export MONGO_DB_CONNECTION=${MONGO_DB_CONNECTION} && "
 mkdir ${QM_DOCKER_PATH}/phpunit
 
 echo "Copying slim/envs/circleci.env to .env"
