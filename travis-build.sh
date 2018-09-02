1650478acc5cb4e1bcd9b6db31e020f2165cdbd5
Mike P. Sinn <m@quantimodo.com>
2018-09-01 22:41:13 -0500

    echo "${WP_LOAD} does not exist"

--- travis-build.sh
+++ travis-build.sh
@@ -38,6 +38,13 @@ cp ${QM_DOCKER_PATH}/slim/envs/circleci.env ${QM_DOCKER_PATH}/.env
 sudo chown -R ${USER} ~/.composer/
 cd slim && composer install --prefer-dist
 cd ${QM_DOCKER_PATH}/public.built && composer install --prefer-dist
+WP_LOAD=${QM_DOCKER_PATH}/public.built/wp/wp-load.php
+if [ ! -e "${WP_LOAD}" ]; then
+    echo "${WP_LOAD} does not exist"
+    exit 1;
+else
+    echo "${WP_LOAD} exists"
+fi
 cd ${QM_DOCKER_PATH}
 set -x
 case "$TEST_SUITE" in
