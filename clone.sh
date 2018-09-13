386344c754822d14585b8de95d509a3bcbaa4880
Mike P. Sinn <m@quantimodo.com>
2018-09-12 21:08:17 -0500

    if [ ! -d "QM-Docker" ]; then echo "Clone of ${BRANCH} branch failed so cloning develop branch" && git clone -b develop --recurse-submodules --single-branch https://${GITHUB_ACCESS_TOKEN}:x-oauth-basic@github.com/mikepsinn/QM-Docker.git QM-Docker; fi

--- clone.sh
+++ clone.sh
@@ -2,4 +2,5 @@
 source ${TRAVIS_BUILD_DIR}/set_environmental_variables.sh
 echo "Checking out revision ${SHA}"
 if [ ! -d "QM-Docker" ]; then echo "Repo not found so cloning" && git clone -b ${BRANCH} --recurse-submodules --single-branch https://${GITHUB_ACCESS_TOKEN}:x-oauth-basic@github.com/mikepsinn/QM-Docker.git QM-Docker; fi
+if [ ! -d "QM-Docker" ]; then echo "Clone of ${BRANCH} branch failed so cloning develop branch" && git clone -b develop --recurse-submodules --single-branch https://${GITHUB_ACCESS_TOKEN}:x-oauth-basic@github.com/mikepsinn/QM-Docker.git QM-Docker; fi
 cd QM-Docker && git stash && git pull origin ${BRANCH} && git submodule update --init --recursive
