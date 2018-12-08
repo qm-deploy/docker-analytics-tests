3ff0523bf20fa7f6cc647efb170ab9f9b99c2d1d
Mike P. Sinn <m@quantimodo.com>
2018-12-08 10:30:19 -0600

    Removed redundant scripts

--- mark_pending_on_github.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source ${TRAVIS_BUILD_DIR}/set_environmental_variables.sh
-COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s) && echo "=== BUILDING COMMIT: $COMMIT_MESSAGE ==="
-source ${TEST_REPO_PATH}/update-status.sh --sha=${SHA} \
-   --repo=mikepsinn/${REPO_TO_TEST} \
-   --status=pending \
-   --message="Testing $COMMIT_MESSAGE on Travis..." \
-   --context="${TRAVIS_TEST_GROUP}" \
-   --url=https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}
